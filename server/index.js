12f6e812232d99486b13d618e81a88f26be7b05b
shinehtetnaing <shinehtetnaing1998@gmail.com>
2023-02-28 15:48:07 +0630

    allow cors

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const steakRoutes = require("./routes/steak");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 // routes
